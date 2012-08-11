3e03c76f719e99a87f35073ea0f166f29dc0759a
Mike Calhoun <mcalhoun@centresource.com>
2012-08-11 12:00:05 -0500

    initial commit, not usable

--- Rakefile
+++ Rakefile
@@ -17,10 +17,10 @@ Jeweler::Tasks.new do |gem|
   gem.name = "soft-deletable"
   gem.homepage = "http://github.com/comike011/soft-deletable"
   gem.license = "MIT"
-  gem.summary = %Q{TODO: one-line summary of your gem}
-  gem.description = %Q{TODO: longer description of your gem}
-  gem.email = "mcalhoun@centresource.com"
-  gem.authors = ["Mike Calhoun"]
+  gem.summary = %Q{Makes items soft deletable}
+  gem.description = %Q{BONG BONG BONG 420}
+  gem.email = "mikedcalhoun@gmail.com"
+  gem.authors = ["Mike Calhoun", "Travis Roberts"]
   # dependencies defined in Gemfile
 end
 Jeweler::RubygemsDotOrgTasks.new
@@ -30,14 +30,6 @@ Rake::TestTask.new(:test) do |test|
   test.libs << 'lib' << 'test'
   test.pattern = 'test/**/test_*.rb'
   test.verbose = true
-end
-
-require 'rcov/rcovtask'
-Rcov::RcovTask.new do |test|
-  test.libs << 'test'
-  test.pattern = 'test/**/test_*.rb'
-  test.verbose = true
-  test.rcov_opts << '--exclude "gems/*"'
 end
 
 task :default => :test
---
+++ lib/soft-deletable.rb
@@ -0,0 +1,53 @@
+module SoftDeleteable
+  def self.included(klass)
+    klass.send(:extend, MacroMethods)
+  end
+
+  module InstanceMethods
+    def soft_delete!
+      update_attribute :deleted_at, Time.zone.now
+
+      # soft_delete all dependencies (down the chain)
+      self.class.aasd_dependents.each do |assoc|
+        self.send(assoc).each do |r|
+          r.soft_delete!
+        end
+      end
+    end
+
+    def recover!
+      update_attribute :deleted_at, nil
+
+      # recover all dependencies
+      self.class.aasd_dependents.each do |assoc|
+        self.send(assoc).unscoped.deleted.each do |r|
+          r.recover!
+        end
+      end
+
+      # recover all depends_on
+      self.class.aasd_depends_on.each do |assoc|
+        self.send(assoc).recover! if self.send(assoc).deleted?
+      end
+    end
+
+    def deleted?
+      deleted_at.present?
+    end
+  end
+
+  module MacroMethods
+    def acts_as_soft_deleteable(opts={})
+      cattr_accessor :aasd_dependents, :aasd_depends_on, :table_name
+
+      self.aasd_dependents = opts[:dependents] || {}
+      self.aasd_depends_on = opts[:depends_on] || {}
+      self.table_name = opts[:table_name] || self.to_s.tableize
+      self.send(:include, InstanceMethods)
+
+      # need to add the table name to the query to avoid ambiguous columns
+      default_scope where("#{table_name}.deleted_at IS NULL")
+      scope :deleted, where("#{table_name}.deleted_at IS NOT NULL")
+    end
+  end
+end
